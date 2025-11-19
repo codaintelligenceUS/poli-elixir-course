4580847bcd5b30486cdac088a38ce7834286ce6e
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2025-11-19 14:45:46 +0200

    chore(gen-server): add async functions
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- lib/03_gen-server/lib/ttl_cache.ex
+++ lib/03_gen-server/lib/ttl_cache.ex
@@ -15,6 +15,15 @@ defmodule ElixirBeginnerCourse.GenServer.TTLCache do
 
   @doc """
   Store a key/value pair for a given TTL in milliseconds.
+
+  This is a synchronous operation - waits for confirmation.
+  If the key already exists, overwrites it and cancels the old timer.
+
+  ## Examples
+
+      iex> {:ok, cache} = TTLCache.start_link()
+      iex> TTLCache.put(cache, :user_id, 123, 5000)
+      :ok
   """
   @spec put(GenServer.server(), term(), term(), non_neg_integer()) :: :ok
   def put(server \\ __MODULE__, key, value, ttl_ms) do
@@ -22,10 +31,70 @@ defmodule ElixirBeginnerCourse.GenServer.TTLCache do
   end
 
   @doc """
-  Fetch a key. Returns {:ok, value} or :error.
+  Fetch a key. Returns `{:ok, value}` or `:error`.
+
+  This is a synchronous operation.
+
+  ## Examples
+
+      iex> {:ok, cache} = TTLCache.start_link()
+      iex> TTLCache.put(cache, :key, "value", 1000)
+      iex> TTLCache.get(cache, :key)
+      {:ok, "value"}
+      iex> TTLCache.get(cache, :missing)
+      :error
   """
   @spec get(GenServer.server(), term()) :: {:ok, term()} | :error
   def get(server \\ __MODULE__, key) do
+    raise "not implemented"
+  end
+
+  @doc """
+  Asynchronously store a key/value pair. Does not wait for confirmation.
+
+  Useful when you don't need to know if the operation succeeded.
+
+  ## Examples
+
+      iex> {:ok, cache} = TTLCache.start_link()
+      iex> TTLCache.put_async(cache, :key, "value", 1000)
+      :ok
+  """
+  @spec put_async(GenServer.server(), term(), term(), non_neg_integer()) :: :ok
+  def put_async(server \\ __MODULE__, key, value, ttl_ms) do
+    raise "not implemented"
+  end
+
+  @doc """
+  Asynchronously delete a key. Does not wait for confirmation.
+
+  Cancels the timer if the key exists.
+
+  ## Examples
+
+      iex> {:ok, cache} = TTLCache.start_link()
+      iex> TTLCache.put(cache, :key, "value", 1000)
+      iex> TTLCache.delete(cache, :key)
+      :ok
+  """
+  @spec delete(GenServer.server(), term()) :: :ok
+  def delete(server \\ __MODULE__, key) do
+    raise "not implemented"
+  end
+
+  @doc """
+  Asynchronously clear all entries. Does not wait for confirmation.
+
+  Cancels all active timers.
+
+  ## Examples
+
+      iex> {:ok, cache} = TTLCache.start_link()
+      iex> TTLCache.clear(cache)
+      :ok
+  """
+  @spec clear(GenServer.server()) :: :ok
+  def clear(server \\ __MODULE__) do
     raise "not implemented"
   end
 
@@ -39,11 +108,45 @@ defmodule ElixirBeginnerCourse.GenServer.TTLCache do
 
   @impl true
   def handle_call({:put, key, value, ttl_ms}, _from, state) do
+    # TODO:
+    # 1. Check if key exists and cancel old timer if present
+    # 2. Schedule a new expiration message with Process.send_after/3
+    # 3. Store {value, timer_ref} in state
+    # 4. Reply with :ok
     raise "not implemented"
   end
 
   @impl true
   def handle_call({:get, key}, _from, state) do
+    # TODO:
+    # 1. Look up the key in state
+    # 2. Return {:ok, value} if found, :error otherwise
+    # 3. Don't modify state
+    raise "not implemented"
+  end
+
+  @impl true
+  def handle_cast({:put, key, value, ttl_ms}, state) do
+    # TODO: Same logic as handle_call(:put, ...) but:
+    # 1. Use {:noreply, new_state} instead of {:reply, :ok, new_state}
+    # 2. This is the async version - no reply needed
+    raise "not implemented"
+  end
+
+  @impl true
+  def handle_cast({:delete, key}, state) do
+    # TODO:
+    # 1. Check if key exists and cancel timer if present
+    # 2. Remove key from state
+    # 3. Return {:noreply, new_state}
+    raise "not implemented"
+  end
+
+  @impl true
+  def handle_cast(:clear, state) do
+    # TODO:
+    # 1. Cancel all active timers (iterate through state)
+    # 2. Return empty state: {:noreply, %{}}
     raise "not implemented"
   end
 
